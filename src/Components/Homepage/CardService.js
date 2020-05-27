c3e0a2da89a1fe63cb3461c79c5b6a97cbf97fd5
Alan <alan@Alans-MacBook-Pro.local>
2020-05-28 02:10:53 +0530

    movile cards

--- src/Components/Homepage/CardService.js
+++ src/Components/Homepage/CardService.js
@@ -9,15 +9,30 @@ export default function CardService(props) {
   //     )
   const handleClick =()=>{
     console.log("ck");
-    const desc = document.querySelector(`#desc${props.num}`);
+    let desc = document.querySelector(`#desc${props.num}`);
     desc.classList.toggle("hidden");
+    let id = document.querySelector(`#id${props.num}`);
+    id.classList.toggle("flex-col");
+
+    for(let i=1;i<=6;i++){
+      if(i==props.num) continue;
+      console.log(i);
+       desc = document.querySelector(`#desc${i}`);
+      desc.classList.add("hidden");
+       id = document.querySelector(`#id${i}`);
+      id.classList.remove("flex-col");
+
+    }
+
   }
   return (
-    <div  onClick={()=>{handleClick()}} className="flex justify-end flex-left bg-gray-100 
-            bg-opacity-75 sm:flex-col sm:h-full h-full mx-12 sm:py-6 py-3 
+    <div  onClick={()=>{handleClick()}} id={`id${props.num}`} className="flex  items-center sm:flex-row 
+              justify-end flex-left bg-gray-100 
+              bg-opacity-75 sm:flex-col sm:h-full h-full mx-12 sm:py-6 py-3 
               sm:px-0 px-1 my-4 sm:my-8 sm:text-center justify-center rounded-lg 
                cardt animate w-9/12 md:w-4/12 xl:w-3/12">
-      <img className="sm:h-12 h-6 sm:mx-auto mx-2" src={Wrench} />
+
+      <img className="sm:h-12 h-6  sm:mx-auto mx-2" src={Wrench} />
       <h1 className="sm:items-center sm:pt-4 sm:pb-2 sm:text-xl text-lg  ">
         {props.title}
       </h1>
