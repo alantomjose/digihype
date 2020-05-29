d0ca0ef85110f80464a66e2d4c13c08304f2addd
ashinlaurel <ashinlaurel1999@gmail.com>
2020-05-29 17:43:03 +0530

    Styling of Blog Post

--- frontend/src/Components/Blog/Post.js
+++ frontend/src/Components/Blog/Post.js
@@ -22,16 +22,25 @@ export default function Post(props) {
   }, []);
 
   return (
-    <div>
-      <div className="mx-2 md:mx-12 lg:mx-32 item-center ">
+    <div
+      className=" w-screen bg-fixed bg-center bg-cover h-full overflow-hidden"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1502920970741-47c1bafc8d49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')",
+      }}
+    >
+      <div className="mx-2 md:mx-12 lg:mx-32 item-center overflow-hidden">
         <div className="h-32 w-full  "> </div>
-        <div className="shadow-lg my-24 ">
+        <div className="shadow-lg my-24 bg-gray-100 bg-opacity-50 rounded ">
           <div
-            className="text-3xl py-6 px-20"
+            className="text-2xl leading-tight font-heading sm:text-3xl py-6 sm:px-20 px-8"
             dangerouslySetInnerHTML={{ __html: title }}
           />
           {/* <div className="px-20 pb-10">{content}</div> */}
-          <div dangerouslySetInnerHTML={{ __html: content }} />
+          <div
+            className="px-10 pb-6 sm:px-20 sm:pb-10"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
         </div>
       </div>
     </div>
