6375ecc598eb4b293e39a7bffe3bda562e5f5ecb
ashinlaurel <ashinlaurel1999@gmail.com>
2020-05-31 00:18:02 +0530

    merge

--- frontend/src/Components/Homepage/BlogSum.js
+++ frontend/src/Components/Homepage/BlogSum.js
@@ -29,7 +29,7 @@ export default function BlogSum() {
   return (
     <div
       id="blogstart"
-      className="py-6 px-1 sm:px-8 h-screen  w-fixed bg-fixed bg-center bg-cover "
+      className="py-6 px-1 sm:px-8 h-screen w-fixed bg-fixed bg-center bg-cover "
       style={{
         backgroundImage:
           "url('https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')",
@@ -50,7 +50,7 @@ export default function BlogSum() {
       <div className="overflow-x-auto">
         <div
           id="blogcards"
-          className="opacity-0 slowanimate flex sm:justify-center justify-between items-around flex-wrap sm:mt-24 "
+          className="opacity-0 slowanimate flex sm:justify-center justify-between items-around w-full sm:mt-24 "
         >
           <BlogSumCard
             title="The Coldest Sunset"
