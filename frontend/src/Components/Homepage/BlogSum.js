3f4e9665761e45415fe611bc554dc281e8e2f97a
Alan <alan@Alans-MacBook-Pro.local>
2020-05-30 23:49:13 +0530

    x-scroll

--- frontend/src/Components/Homepage/BlogSum.js
+++ frontend/src/Components/Homepage/BlogSum.js
@@ -29,21 +29,25 @@ export default function BlogSum() {
   return (
     <div
       id="blogstart"
-      className="py-6 px-6 sm:px-8 h-screen overflow-y-auto w-screen bg-fixed bg-center bg-cover "
+      className="py-6 px-1 sm:px-8 h-screen  w-fixed bg-fixed bg-center bg-cover relative"
       style={{
         backgroundImage:
           "url('https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')",
       }}
     >
+    <div id="nextLogo" className="absolute text-4xl animate  font-heading text-yellow-300 "> 
+    <i className="fa m-1   fa-arrow-right"></i>
+     </div>
       <h1
         id="blogheading"
         className="opacity-0 slowanimate sm:text-5xl text-3xl my-3 text-center font-heading"
       >
         Our Blog
       </h1>
+      <div className="overflow-x-auto">
       <div
         id="blogcards"
-        className="opacity-0 slowanimate flex justify-center items-around flex-wrap sm:mt-24 "
+        className="opacity-0 slowanimate flex justify-between items-around flex-wrap sm:mt-24 "
       >
         <BlogSumCard
           title="The Coldest Sunset"
@@ -60,6 +64,7 @@ export default function BlogSum() {
           desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
           link="enterlinktopost"
         />
+      </div>
       </div>
       <div className=" text-center m-10 ">
         <Link
