40a75e4a430ce6887433d1452cd3ed63e71494db
ashinlaurel <ashinlaurel1999@gmail.com>
2020-06-11 19:01:27 +0530

    added proxy to firebase functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "./build/tailwind.css";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 import About from "./Components/About";
@@ -9,6 +10,9 @@ import Main from "./Components/Blog/Main";
 import Post from "./Components/Blog/Post";
 import SocialMedia from "./Components/SocialMedia";
 import Loading from "./Components/Loading";
+
+axios.defaults.baseURL =
+  "https://us-central1-hypedigi-app.cloudfunctions.net/app";
 
 function App() {
   const [loading, setLoading] = useState("true");
